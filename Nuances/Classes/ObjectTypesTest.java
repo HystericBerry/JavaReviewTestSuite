37fce31f3e90ef94b4004d49ce1c0e530d71ea2b
HystericBerry <pkim7662@gmail.com>
2017-05-25 20:39:40 -0700

    Callback Test added.

--- Nuances/Classes/ObjectTypesTest.java
+++ Nuances/Classes/ObjectTypesTest.java
@@ -1,11 +1,23 @@
 package Classes;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+
+import java.awt.Rectangle;
 
 import org.junit.Test;
 
+import Math.MathUtils;
+import Classes.CallbackExample.IntegerMeasurer;
+import Classes.CallbackExample.RectMeasurer;
+import Structs.DuplicateStruct.Child;
+import Structs.DuplicateStruct.Parent;
+import Structs.InvalidLocalVar;
+
 public class ObjectTypesTest
 {
+	private static double epsilon = 1e-6;
 	@Test
 	public void testInvalidLocalVariable()
 	{
@@ -17,20 +29,40 @@ public class ObjectTypesTest
 		assertNotEquals( expectedValue, actualValue );
 	}
 	
-	/**
-	 * This Class demonstrates the fact that the Java Language applies greater 
-	 * precedence to local variables over member variables that have the same name.
-	 * 
-	 * @author pkim7
-	 */
-	private class InvalidLocalVar
+	@Test
+	public void testInheritDuplicateFields()
 	{
-		@SuppressWarnings("all")
-		public InvalidLocalVar( int invalid )
-		{
-			invalid = invalid;
-		}
+		String expectedObj = "Hello World";
+		Parent parent = new Parent(expectedObj);
+		Child child = new Child(expectedObj);
+		Parent childInst = (Parent) child;
 		
-		public int invalid;
+		assertEquals( expectedObj, parent.toString() );
+		assertNull( child.toString() );
+		assertNull( childInst.toString() );
+	}
+	
+	@Test
+	public void testCallback()
+	{
+		RectMeasurer rectMeas = new RectMeasurer();
+		Object[] arr = new Rectangle[] {
+				new Rectangle(5, 10, 20, 30),
+				new Rectangle(10, 20, 30, 40),
+				new Rectangle(20, 30, 5, 15)
+		};
+		
+		double expectedVal = 625;
+		double actualVal = MathUtils.average(arr, rectMeas);
+		assertEquals( expectedVal, actualVal, epsilon );
+		
+		
+		IntegerMeasurer intMeas = new IntegerMeasurer();
+		arr = new Integer[] { new Integer(1), new Integer(2),
+				new Integer(3), new Integer(4) };
+		
+		expectedVal = 10.0 / 4.0;
+		actualVal = MathUtils.average(arr, intMeas);
+		assertEquals( expectedVal, actualVal, epsilon );
 	}
 }
